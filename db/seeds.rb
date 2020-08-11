ab6bab170a1203d4d25cbe8bfd87b68d320cb3ad
BenWix <44454993+BenWix@users.noreply.github.com>
2020-08-11 11:13:27 -0400

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ben = User.create(name: 'Ben', email: 'ben@ben.com', phone: '317')
+meghan = User.create(name: 'Meghan', email: 'meghan@meghan.com', phone: '260')
+will = User.create(name: 'Will', email: 'will@will.com', phone: '502')
+mason = User.create(name: 'Mason', email: 'mason@mason.com', phone: '692')
+olivia = User.create(name: 'Olivia', email: 'olivia@olivia.com', phone: '442')
+cheyenne = User.create(name: 'Cheyenne', email: 'cheyenne@cheyenne.com', phone: '273')
