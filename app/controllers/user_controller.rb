e502dece95e046317126067676fb84dea23a9e82
BenWix <44454993+BenWix@users.noreply.github.com>
2020-08-11 11:20:08 -0400

    updates routes and users controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,13 @@
 class UserController < ApplicationController
+
+    def index 
+        users = User.all
+        render json: users
+    end
+
+    def create 
+        user = User.create(name: params[:name], email: params[:email], phone: params[:phone])
+    
+        render json: user
+    end
 end
