7409a5c01bea4f0975461d000aae2af0d8039d3e
BenWix <44454993+BenWix@users.noreply.github.com>
2020-08-12 14:36:34 -0400

    add bikes to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     def show 
         user = User.find_by_id(params[:id])
-        render json: user
+        render json: user, include: [:bikes]
     end
 
 
