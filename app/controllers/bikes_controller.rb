b938d2876fdeb3447056a8af4f01ed49e927b5bf
BenWix <44454993+BenWix@users.noreply.github.com>
2020-08-13 10:35:04 -0400

    updates naming convention

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,15 @@
+class BikesController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+
+    def index 
+        bikes = Bike.all
+        render json: bikes
+    end
+    
+    def create
+        user = User.find_by_id(params[:user_id])
+        bike = user.bikes.create(name: params[:name], miles: params[:miles], bike_type: params[:bike_type], strava_id: params[:strava_id])
+        render json: bike
+    end
+
+end
